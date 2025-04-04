2b37b8b9dc36793cd857bb867758baa0c8d4f072
Meryl Marasigan <meryl0729@gmail.com>
2025-04-03 17:28:43 -0700

    Added App, Footer, Header, and Note components

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    let year = new Date().getFullYear();
+
+    return (
+    <footer>
+        <p>Copyright © {year}</p>
+    </footer>);
+}
+ 
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -10,3 +10,9 @@
 //https://l1pp6.csb.app/
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
