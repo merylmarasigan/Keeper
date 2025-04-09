42d53105d6e353d0d397caf056c600d3754651d7
Meryl Marasigan <meryl0729@gmail.com>
2025-04-08 21:09:45 -0700

    added ability to map data to functional components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header'
 import Footer from './Footer.js'
 import Note from './Note.js';
 import './index.css'
+import notes from './notes.js';
 
 
 
@@ -11,7 +12,12 @@ function App() {
   return (
     <div>
       <Header/>
-      <Note/>
+      {notes.map((note) =>{
+        return (
+          <Note key={note.key} title={note.title} content={note.content}/>
+        );
+
+      })}
       <Footer/>
     </div>
     
--- src/Note.js
+++ src/Note.js
@@ -1,9 +1,9 @@
 
-const Note = () => {
+const Note = (props) => {
     return (
         <div className='note'>
-            <h1>This is the note title </h1>
-            <p>this is the note content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
